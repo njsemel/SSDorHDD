b24d62575447d5fa2732867ad13c972380f4ccbd
njsemel <nathanieljsemel@gmail.com>
2024-09-01 12:09:33 -0400

    include release build files

--- obj/Release/net8.0-windows/SSDorHDD.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/SSDorHDD.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SSDorHDD")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99bc9ed68d8c5a1cbb6b339b01c46f4eacde82e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SSDorHDD")]
